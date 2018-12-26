fd52be2fb8fd0ff348f5c3f6cba8c7650c6e54c9
MOHD DANISH <46101487+THENISH12@users.noreply.github.com>
2018-12-26 10:51:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body>
 <div class="col-md-6" style="margin-left:25%; margin-right:25%; padding-top:50px" align="center"  >
 <h1>Log In</h1><br>
-<form action="index.php" method="post" >
+<form action="connect.php" method="post" >
 <input type="email" name="email" value="" placeholder="Enter Email" style="width:300px ; height:30px" required /><br><br>
 <input type="password" name="password" value="" placeholder="Enter Password" style="width:300px ; height:30px" required  /><br><br>
 <button type="submit" name="submit" value="submit" style="width:300px ; height:30px ; background-color:DC8179" >Log in</button><br>
